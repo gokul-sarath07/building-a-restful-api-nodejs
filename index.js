9c590f7484ea7b8f168219618665417ae8eece2f
Gokul Sarath <gokul510.s@gmail.com>
2022-04-15 23:21:21 +0530

    Parsing Request Paths.

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+   Primary file for the api.
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should respond to all requests with a string.
+const server = http.createServer((req, res) => {
+
+   // Parse the url.
+   const parsedUrl = url.parse(req.url, true);
+
+   // Get the path.
+   const path = parsedUrl.pathname;
+   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+   // Send the response.
+   res.end("Hello World!");
+
+   // Log the request path.
+   console.log('Request received on path: ' + trimmedPath);
+})
+
+// Start the server, and have it listen on port 3000.
+server.listen(3000, () => {
+   console.log("The server is listening on port 3000 now.");
+})
